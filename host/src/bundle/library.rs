967f418a359f0324881e1c62cb48d7224443ea9b
Adrien Prokopowicz <6529475+prokopyl@users.noreply.github.com>
2023-11-25 23:26:36 +0100

    Fix concurrently loading a bundle multiple times (#16)
    * Fix concurrently loading a bundle multiple times
    
    * Optimize some ordering
    
    * Reduce some iterations
    
    * Minor cleanup

---
+++ host/src/bundle/library.rs
@@ -0,0 +1,51 @@
+use crate::bundle::PluginBundleError;
+use clack_common::entry::EntryDescriptor;
+use libloading::Library;
+use stable_deref_trait::StableDeref;
+use std::ffi::OsStr;
+use std::ops::Deref;
+use std::ptr::NonNull;
+
+pub(crate) struct PluginEntryLibrary {
+    _library: Library,
+    entry_ptr: NonNull<EntryDescriptor>,
+}
+
+const SYMBOL_NAME: &[u8] = b"clap_entry\0";
+impl PluginEntryLibrary {
+    pub fn load(path: &OsStr) -> Result<Self, PluginBundleError> {
+        let library =
+            unsafe { Library::new(path) }.map_err(PluginBundleError::LibraryLoadingError)?;
+
+        let symbol = unsafe { library.get::<*const EntryDescriptor>(SYMBOL_NAME) }
+            .map_err(PluginBundleError::LibraryLoadingError)?;
+
+        let entry_ptr = NonNull::new(*symbol as *mut EntryDescriptor)
+            .ok_or(PluginBundleError::NullEntryPointer)?;
+
+        Ok(Self {
+            _library: library,
+            entry_ptr,
+        })
+    }
+
+    #[inline]
+    pub fn entry(&self) -> &EntryDescriptor {
+        unsafe { self.entry_ptr.as_ref() }
+    }
+}
+
+impl Deref for PluginEntryLibrary {
+    type Target = EntryDescriptor;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.entry()
+    }
+}
+
+unsafe impl StableDeref for PluginEntryLibrary {}
+
+// SAFETY: Entries and factories are all thread-safe by the CLAP spec
+unsafe impl Send for PluginEntryLibrary {}
+unsafe impl Sync for PluginEntryLibrary {}
--- host/tests/loading.rs
+++ host/tests/loading.rs
@@ -19,3 +19,29 @@ pub fn it_works() {
         .unwrap();
     assert_eq!(desc.id().unwrap().to_bytes(), b"org.rust-audio.clack.gain");
 }
+
+#[test]
+#[cfg_attr(miri, ignore)] // Miri does not support calling foreign function (dlopen)
+pub fn it_works_concurrently() {
+    let bundle_path = format!(
+        "{}/../target/debug/{}gain{}",
+        env!("CARGO_MANIFEST_DIR"),
+        std::env::consts::DLL_PREFIX,
+        std::env::consts::DLL_SUFFIX
+    );
+
+    std::thread::scope(|s| {
+        for _ in 0..300 {
+            s.spawn(|| {
+                let bundle = PluginBundle::load(&bundle_path).unwrap();
+
+                let desc = bundle
+                    .get_factory::<PluginFactory>()
+                    .unwrap()
+                    .plugin_descriptor(0)
+                    .unwrap();
+                assert_eq!(desc.id().unwrap().to_bytes(), b"org.rust-audio.clack.gain");
+            });
+        }
+    })
+}
