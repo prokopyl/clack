fd5e85faa1993c841389fe92b79e6c79c5f62e35
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2024-05-06 10:41:51 +0200

    Fully document the UnknownEvent type

--- common/src/events.rs
+++ common/src/events.rs
@@ -105,37 +105,105 @@ pub unsafe trait Event: AsRef<UnknownEvent> + Sized + 'static {
     }
 }
 
+/// An event of an undetermined type.
+///
+/// CLAP event types come in a variety of different sizes. Therefore, unknown events do not have
+/// their sizes known at compile-time, making this type a
+/// [DST](https://doc.rust-lang.org/reference/dynamically-sized-types.html) which can only be
+/// operated on through references (`&`).
+///
+/// Moreover, because CLAP supports defining using custom events, it's possible for any host or
+/// plugin to encounter an event type it cannot possibly figure out the concrete type of.
+///
+/// The [`UnknownEvent`] type allows to safely work with events without knowing their type.
+/// Their headers can be retrieved with the [`header()`](UnknownEvent::header) method, and they can
+/// be directly copied around through [`InputEventBuffer`s](io::InputEventBuffer) and
+/// [`OutputEventBuffer`s](io::OutputEventBuffer).
+///
+/// To try and figure out an event's concrete type, the [`as_event()`](UnknownEvent::as_event)
+/// method can be used to try and downcast it to a specific event type. The
+/// [`as_core_event`](UnknownEvent::as_core_event) can also be used to check if the event is a
+/// standard CLAP event, and then further `match`ed to find its specific type.
+///
+/// Alternatively, one can also use the [`as_event_for_space`](UnknownEvent::as_event_for_space) and
+/// [`as_event_space`](UnknownEvent::as_event_space) methods, which are respectively identical but
+/// for custom [event spaces](EventSpace), by checking against an ID from the `event_registry` extension.
 #[repr(transparent)]
 pub struct UnknownEvent {
     data: [u8],
 }
 
 impl UnknownEvent {
-    /// Gets an unknown event from a raw event header.
-    ///
-    /// # Safety
-    /// The caller *must* ensure that not only the contents of the header are valid, but also that
-    /// they are immediately preceding the rest of the event struct matching the event and space IDs
-    /// in the header.
-    #[inline]
-    pub const unsafe fn from_raw<'e>(header: *const clap_event_header) -> &'e Self {
-        // SAFETY: no need to check for len > 0, since the data slice includes the header itself.
-        let data = core::slice::from_raw_parts(header as *const _, (*header).size as usize);
-        // SAFETY: The caller guarantees the right number of bytes is available after the given pointer in the size field.
-        Self::from_bytes_unchecked(data)
-    }
-
-    #[inline]
-    pub const fn as_raw(&self) -> *const clap_event_header {
-        self.data.as_ptr() as *const clap_event_header
-    }
-
+    /// Returns the header of this event.
     #[inline]
     pub const fn header(&self) -> &EventHeader {
         // SAFETY: Pointer is guaranteed to be valid from constructors
         unsafe { EventHeader::from_raw(&*self.as_raw()) }
     }
 
+    /// Attempts to downcast this event to a specific standard CLAP event type.
+    ///
+    /// This returns a down-casted reference to the event if the event matches the given type, or
+    /// `None` if it doesn't.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use clack_common::events::event_types::NoteOnEvent;
+    /// use clack_common::events::UnknownEvent;
+    ///
+    /// fn handle_event(event: &UnknownEvent) {
+    ///     if let Some(note_on_event) = event.as_event::<NoteOnEvent>() {
+    ///         // Handle the Note On event
+    ///         # let _ = note_on_event;
+    ///     } else {
+    ///         // This is not a Note On event
+    ///     }
+    /// }
+    ///
+    /// ```
+    #[inline]
+    pub fn as_event<'s, E: Event<EventSpace<'s> = CoreEventSpace<'s>>>(&self) -> Option<&E> {
+        self.as_event_for_space(EventSpaceId::core())
+    }
+
+    /// Attempts to downcast this event to the [core event space](CoreEventSpace), allowing it to be
+    /// match against all the standard CLAP event types.
+    ///
+    /// This returns `None` instead if the given event doesn't match the known standard events.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use clack_common::events::event_types::NoteOnEvent;
+    /// use clack_common::events::UnknownEvent;
+    ///
+    /// fn handle_event(event: &UnknownEvent) {
+    ///     use clack_common::events::spaces::CoreEventSpace::*;
+    ///
+    ///     let Some(core_event) = event.as_core_event() else {
+    ///         // This is not a known standard event.
+    ///         return;
+    ///     };
+    ///
+    ///     match core_event {
+    ///         NoteOn(e) => { /* Handle the Note On event */ }
+    ///         NoteOff(e) => { /* Handle the Note Off event */ }
+    ///         ParamValue(e) => { /* Handle the Parameter Value event */ }
+    ///         _ => { /* This is another event type than the ones we chose to handle */ }
+    ///     }
+    /// }
+    ///
+    /// ```
+    #[inline]
+    pub fn as_core_event(&self) -> Option<CoreEventSpace> {
+        self.as_event_space(EventSpaceId::core())
+    }
+
+    /// Attempts to downcast this event to a specific event type from a given [event space](EventSpace).
+    ///
+    /// This returns a down-casted reference to the event if the event matches the given type and
+    /// space, or `None` if it doesn't.
     #[inline]
     pub fn as_event_for_space<E: Event>(
         &self,
@@ -153,9 +221,19 @@ impl UnknownEvent {
         Some(unsafe { self.as_event_unchecked() })
     }
 
+    /// Attempts to downcast this event to the given [event space](EventSpace), allowing it to be
+    /// match against all the event types of that space.
+    ///
+    /// This returns `None` instead if the given event doesn't match the known events from that
+    /// event space.
     #[inline]
-    pub fn as_event<'s, E: Event<EventSpace<'s> = CoreEventSpace<'s>>>(&self) -> Option<&E> {
-        self.as_event_for_space(EventSpaceId::core())
+    pub fn as_event_space<'s, S: EventSpace<'s>>(&'s self, space_id: EventSpaceId<S>) -> Option<S> {
+        if space_id.id() != self.header().space_id()?.id() {
+            return None;
+        }
+
+        // SAFETY: we just checked the event space ID is valid for the current event.
+        unsafe { S::from_unknown(self) }
     }
 
     /// Casts this event as an event of a given type, without performing any checks.
@@ -168,27 +246,35 @@ impl UnknownEvent {
         &*(self as *const _ as *const E)
     }
 
+    /// Gets an unknown event from a raw event header.
+    ///
+    /// # Safety
+    /// The caller *must* ensure that not only the contents of the header are valid, but also that
+    /// they are immediately preceding the rest of the event struct matching the event and space IDs
+    /// in the header.
     #[inline]
-    pub fn as_core_event(&self) -> Option<CoreEventSpace> {
-        self.as_event_space(EventSpaceId::core())
+    pub const unsafe fn from_raw<'e>(header: *const clap_event_header) -> &'e Self {
+        // SAFETY: no need to check for len > 0, since the data slice includes the header itself.
+        let data = core::slice::from_raw_parts(header as *const _, (*header).size as usize);
+        // SAFETY: The caller guarantees the right number of bytes is available after the given pointer in the size field.
+        Self::from_bytes_unchecked(data)
     }
 
+    /// Returns a raw, C-FFI compatible event header pointer to this event.
     #[inline]
-    pub fn as_event_space<'s, S: EventSpace<'s>>(&'s self, space_id: EventSpaceId<S>) -> Option<S> {
-        if space_id.id() != self.header().space_id()?.id() {
-            return None;
-        }
-
-        // SAFETY: we just checked the event space ID is valid for the current event.
-        unsafe { S::from_unknown(self) }
+    pub const fn as_raw(&self) -> *const clap_event_header {
+        self.data.as_ptr() as *const clap_event_header
     }
 
+    /// Returns the event as a raw byte buffer. This includes the event's (header)[EventHeader].
     #[inline]
     pub fn as_bytes(&self) -> &[u8] {
         &self.data
     }
 
     /// Retrieves an event from a byte buffer, without performing any checks.
+    ///
+    /// The given buffer must include the event's (header)[EventHeader].
     ///
     /// # Safety
     ///
