057844326c4085c9c8efb238925cfede56bb41a5
Adrien Prokopowicz <adrien.prokpowicz@gmail.com>
2025-12-04 06:43:46 +0100

    Add some docs

--- extensions/src/clap_wrapper.rs
+++ extensions/src/clap_wrapper.rs
@@ -1,4 +1,7 @@
-#![deny(missing_docs)]
+//! Extensions used by the [`clap-wrapper`] project to provide additional features and metadata to
+//! CLAP plugins when they are wrapped and presented as AUv2 or VST3 plugins.
+//!
+//! [`clap-wrapper`]: https://github.com/free-audio/clap-wrapper
 
 pub mod auv2;
 pub mod vst3;
