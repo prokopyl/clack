814f0e77f6a3297dd2a0228a1e7d1d01da1cd337
Adrien Prokopowicz <6529475+prokopyl@users.noreply.github.com>
2026-01-05 17:58:01 +0100

    Fully document UniversalPluginId & UniversalPluginAbi

--- common/src/utils/universal_plugin_id.rs
+++ common/src/utils/universal_plugin_id.rs
@@ -6,7 +6,7 @@ use core::ffi::CStr;
 /// Known ABI types that can be used in [`UniversalPluginId`]s.
 ///
 /// This is non-exhaustive, as other plugin ID types may be added in the future.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 #[non_exhaustive]
 pub enum UniversalPluginAbi {
     /// The CLAP ABI.
@@ -35,7 +35,10 @@ impl UniversalPluginAbi {
 
     /// Returns the ABI type matching the given ABI string.
     ///
-    /// If
+    /// If the ABI type string doesn't match a known [`UniversalPluginAbi`] variant, this returns
+    /// [`None`].
+    ///
+    /// This comparison is case-sensitive.
     #[inline]
     pub const fn from_abi_str(abi: &CStr) -> Option<Self> {
         match abi.to_bytes() {
@@ -48,7 +51,10 @@ impl UniversalPluginAbi {
     }
 }
 
-/// A unique identifier that works across plugin
+/// A unique identifier that works across plugin APIs.
+///
+/// This is a ([`abi`](Self::abi), [`id`](Self::id)) pair of borrowed strings that live for the `'a`
+/// lifetime.
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub struct UniversalPluginId<'a> {
     /// The ABI string.
@@ -69,43 +75,77 @@ pub struct UniversalPluginId<'a> {
 }
 
 impl<'a> UniversalPluginId<'a> {
-    pub const unsafe fn from_raw_ptr(ptr: *const clap_universal_plugin_id) -> Option<Self> {
-        if ptr.is_null() {
-            return None;
-        }
-
-        // SAFETY: TODO
-        let plugin_id = unsafe { ptr.read() };
-        Self::from_raw(plugin_id)
-    }
-
+    /// Creates a [`UniversalPluginId`] from its raw, C-FFI compatible representation.
+    ///
+    /// If either of the string pointers within the given struct are `NULL`, this returns [`None`].
+    ///
+    /// # Safety
+    ///
+    /// Either of the contained pointers can be `NULL`, in which case the following requirements do
+    /// not apply.
+    ///
+    /// Both pointers must point to valid C strings, and remain valid for the duration of the `'a` lifetime.
+    ///
+    /// See the documentation of [`CStr::from_ptr`] for the exhaustive list of safety requirements.
+    #[inline]
     pub const unsafe fn from_raw(raw: clap_universal_plugin_id) -> Option<Self> {
         Some(Self {
             abi: if raw.abi.is_null() {
                 return None;
             } else {
-                // SAFETY: TODO
+                // SAFETY: pointer is guaranteed to be valid by caller
                 unsafe { CStr::from_ptr(raw.abi) }
             },
             id: if raw.id.is_null() {
                 return None;
             } else {
-                // SAFETY: TODO
+                // SAFETY: pointer is guaranteed to be valid by caller
                 unsafe { CStr::from_ptr(raw.id) }
             },
         })
     }
 
+    /// Creates a [`UniversalPluginId`] from a pointer to its raw, C-FFI compatible representation.
+    ///
+    /// If the given `ptr`, or either of the string pointers within the pointed struct are `NULL`, this returns [`None`].
+    ///
+    /// This function is a version of the [`from_raw`](Self::from_raw) method, except it allows to read
+    /// directly from a raw pointer without actually borrowing it, which may be slightly safer.
+    ///
+    /// # Safety
+    ///
+    /// The given `ptr` must be well-aligned and valid for that.
+    /// On top of that, the [`clap_universal_plugin_id`] value it points to must also satisfy the
+    /// safety requirements of the [`from_raw`](Self::from_raw) method.
+    #[inline]
+    pub const unsafe fn from_raw_ptr(ptr: *const clap_universal_plugin_id) -> Option<Self> {
+        if ptr.is_null() {
+            return None;
+        }
+
+        // SAFETY: pointer is guaranteed to be valid for reads by caller
+        let plugin_id = unsafe { ptr.read() };
+        Self::from_raw(plugin_id)
+    }
+
+    /// Creates a [`UniversalPluginId`] for a CLAP plugin with a given `id`.
     #[inline]
     pub const fn clap(id: &'a CStr) -> Self {
         Self { abi: c"clap", id }
     }
 
+    /// Returns the [`UniversalPluginAbi`] matching this ID's plugin ABI.
+    ///
+    /// If the ABI type string doesn't match a known [`UniversalPluginAbi`] variant, this returns
+    /// [`None`].
     #[inline]
     pub const fn abi(&self) -> Option<UniversalPluginAbi> {
         UniversalPluginAbi::from_abi_str(self.abi)
     }
 
+    /// Returns a raw, C-FFI compatible representation of this ID.
+    ///
+    /// The pointers contained in this struct are valid for the `'a` lifetime.
     #[inline]
     pub const fn to_raw(&self) -> clap_universal_plugin_id {
         clap_universal_plugin_id {
