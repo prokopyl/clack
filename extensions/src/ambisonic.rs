aa2315339c1d92349568f4fcc4c12ffafeec43c1
Quant1um <blep@amee.ee>
2026-01-18 21:32:39 +0400

    add 'configurable-audio-ports' extension, 'surround' and 'ambisonic' extensions

---
+++ extensions/src/ambisonic.rs
@@ -0,0 +1,137 @@
+// #![deny(missing_docs)]
+
+//! This extension can be used to specify the ambisonic channel mapping ([`AmbisonicConfig`]) used by the plugin.
+
+use crate::audio_ports::AudioPortType;
+use clack_common::extensions::{Extension, HostExtensionSide, PluginExtensionSide, RawExtension};
+use clap_sys::ext::ambisonic::*;
+use std::ffi::CStr;
+
+/// The Plugin-side of the Ambisonic extension.
+#[derive(Copy, Clone)]
+#[allow(dead_code)]
+pub struct PluginAmbisonic(RawExtension<PluginExtensionSide, clap_plugin_ambisonic>);
+
+/// The Host-side of the Ambisonic extension.
+#[derive(Copy, Clone)]
+#[allow(dead_code)]
+pub struct HostAmbisonic(RawExtension<HostExtensionSide, clap_host_ambisonic>);
+
+// SAFETY: The type is repr(C) and ABI-compatible with the matching extension type.
+unsafe impl Extension for PluginAmbisonic {
+    const IDENTIFIERS: &[&CStr] = &[CLAP_EXT_AMBISONIC, CLAP_EXT_AMBISONIC_COMPAT];
+    type ExtensionSide = PluginExtensionSide;
+
+    unsafe fn from_raw(raw: RawExtension<Self::ExtensionSide>) -> Self {
+        // SAFETY: This type is expected to contain a type that is ABI-compatible with the matching extension type.
+        Self(unsafe { raw.cast() })
+    }
+}
+
+// SAFETY: The type is repr(C) and ABI-compatible with the matching extension type.
+unsafe impl Extension for HostAmbisonic {
+    const IDENTIFIERS: &[&CStr] = &[CLAP_EXT_AMBISONIC, CLAP_EXT_AMBISONIC_COMPAT];
+    type ExtensionSide = HostExtensionSide;
+
+    unsafe fn from_raw(raw: RawExtension<Self::ExtensionSide>) -> Self {
+        // SAFETY: This type is expected to contain a type that is ABI-compatible with the matching extension type.
+        Self(unsafe { raw.cast() })
+    }
+}
+
+/// Ambisonic data exchange format for an audio port.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(C)]
+pub struct AmbisonicConfig {
+    /// Ambisonic channel ordering
+    pub ordering: AmbisonicOrdering,
+
+    /// Ambisonic normalization method
+    pub normalization: AmbisonicNormalization,
+}
+
+/// Component ordering for an ambisonic data exchange format.
+#[repr(u32)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum AmbisonicOrdering {
+    /// Furse-Malham channel ordering
+    FuMa = CLAP_AMBISONIC_ORDERING_FUMA,
+    /// Ambisonic Channel Number (ACN) ordering
+    ACN = CLAP_AMBISONIC_ORDERING_ACN,
+}
+
+/// Normalization method for an ambisonic data exchange format.
+#[repr(u32)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum AmbisonicNormalization {
+    /// maxN normalization scheme
+    MaxN = CLAP_AMBISONIC_NORMALIZATION_MAXN,
+    /// Schmidt semi-normalisation (3D)
+    SN3D = CLAP_AMBISONIC_NORMALIZATION_SN3D,
+    /// Schmidt semi-normalisation (2D)
+    SN2D = CLAP_AMBISONIC_NORMALIZATION_SN2D,
+    /// Full 3D normalization
+    N3D = CLAP_AMBISONIC_NORMALIZATION_N3D,
+    /// Full 2D normalization
+    N2D = CLAP_AMBISONIC_NORMALIZATION_N2D,
+}
+
+impl AmbisonicConfig {
+    pub fn from_raw(raw: clap_ambisonic_config) -> Option<Self> {
+        Some(Self {
+            ordering: AmbisonicOrdering::from_raw(raw.ordering)?,
+            normalization: AmbisonicNormalization::from_raw(raw.normalization)?,
+        })
+    }
+
+    pub fn as_raw(&self) -> &clap_ambisonic_config {
+        // SAFETY: This type is repr(C) and ABI-compatible with clap_ambisonic_config, and all of its fields are valid for the corresponding fields in clap_ambisonic_config.
+        unsafe { &*(self as *const Self as *const clap_ambisonic_config) }
+    }
+}
+
+impl AmbisonicOrdering {
+    pub fn from_raw(raw: u32) -> Option<Self> {
+        match raw {
+            i if i == CLAP_AMBISONIC_ORDERING_FUMA => Some(AmbisonicOrdering::FuMa),
+            i if i == CLAP_AMBISONIC_ORDERING_ACN => Some(AmbisonicOrdering::ACN),
+            _ => None,
+        }
+    }
+
+    pub fn to_raw(self) -> u32 {
+        self as _
+    }
+}
+
+impl AmbisonicNormalization {
+    pub fn from_raw(raw: u32) -> Option<Self> {
+        match raw {
+            i if i == CLAP_AMBISONIC_NORMALIZATION_MAXN => Some(AmbisonicNormalization::MaxN),
+            i if i == CLAP_AMBISONIC_NORMALIZATION_SN3D => Some(AmbisonicNormalization::SN3D),
+            i if i == CLAP_AMBISONIC_NORMALIZATION_SN2D => Some(AmbisonicNormalization::SN2D),
+            i if i == CLAP_AMBISONIC_NORMALIZATION_N3D => Some(AmbisonicNormalization::N3D),
+            i if i == CLAP_AMBISONIC_NORMALIZATION_N2D => Some(AmbisonicNormalization::N2D),
+            _ => None,
+        }
+    }
+
+    pub fn to_raw(self) -> u32 {
+        self as _
+    }
+}
+
+impl AudioPortType<'static> {
+    /// Ambisonic audio port type.
+    pub const AMBISONIC: Self = AudioPortType(CLAP_PORT_AMBISONIC);
+}
+
+#[cfg(feature = "clack-plugin")]
+mod plugin;
+#[cfg(feature = "clack-plugin")]
+pub use plugin::*;
+
+#[cfg(feature = "clack-host")]
+mod host;
+#[cfg(feature = "clack-host")]
+pub use host::*;
---
+++ extensions/src/configurable_audio_ports.rs
@@ -0,0 +1,238 @@
+#![deny(missing_docs)]
+
+//! This extension lets the host configure the plugin's input and output audio ports.
+//! This is a "push" approach to audio ports configuration.
+
+use crate::audio_ports::AudioPortType;
+use clack_common::extensions::{Extension, PluginExtensionSide, RawExtension};
+use clap_sys::ext::configurable_audio_ports::{
+    CLAP_EXT_CONFIGURABLE_AUDIO_PORTS, CLAP_EXT_CONFIGURABLE_AUDIO_PORTS_COMPAT,
+    clap_audio_port_configuration_request, clap_plugin_configurable_audio_ports,
+};
+use std::{
+    ffi::{CStr, c_void},
+    marker::PhantomData,
+    os::raw::c_char,
+    ptr::null,
+};
+
+/// The Plugin-side of the Configurable Audio Ports extension.
+#[derive(Copy, Clone)]
+#[allow(dead_code)]
+pub struct PluginConfigurableAudioPorts(
+    RawExtension<PluginExtensionSide, clap_plugin_configurable_audio_ports>,
+);
+
+// SAFETY: The type is repr(C) and ABI-compatible with the matching extension type.
+unsafe impl Extension for PluginConfigurableAudioPorts {
+    const IDENTIFIERS: &[&CStr] = &[
+        CLAP_EXT_CONFIGURABLE_AUDIO_PORTS,
+        CLAP_EXT_CONFIGURABLE_AUDIO_PORTS_COMPAT,
+    ];
+    type ExtensionSide = PluginExtensionSide;
+
+    unsafe fn from_raw(raw: RawExtension<Self::ExtensionSide>) -> Self {
+        // SAFETY: This type is expected to contain a type that is ABI-compatible with the matching extension type.
+        Self(unsafe { raw.cast() })
+    }
+}
+
+/// A port type and additional information for a configurable audio port request.
+#[derive(Copy, Clone, Debug)]
+#[non_exhaustive]
+pub enum AudioPortsRequestDetails<'a> {
+    /// A request for a mono audio port
+    Mono,
+
+    /// A request for a stereo audio port
+    Stereo,
+
+    /// A request for an untyped port with a set number of channels.
+    Untyped {
+        /// Requested number of channels for this port.
+        channels: u32,
+    },
+
+    /// A request for a surround audio port with a set channel layout.
+    #[cfg(feature = "surround")]
+    Surround {
+        /// Requested surround channel layout.
+        channels: &'a [crate::surround::SurroundChannel],
+    },
+
+    /// A request for an ambisonic audio port with a set ambisonic configuration.
+    #[cfg(feature = "ambisonic")]
+    Ambisonic {
+        /// Requested ambisonic configuration.
+        config: crate::ambisonic::AmbisonicConfig,
+        /// Requested number of channels.
+        channels: u32,
+    },
+}
+
+impl<'a> AudioPortsRequestDetails<'a> {
+    /// Get the requested port type.
+    pub fn port_type(&self) -> Option<AudioPortType<'a>> {
+        match self {
+            AudioPortsRequestDetails::Mono => Some(AudioPortType::MONO),
+            AudioPortsRequestDetails::Stereo => Some(AudioPortType::STEREO),
+            AudioPortsRequestDetails::Untyped { .. } => None,
+            #[cfg(feature = "surround")]
+            AudioPortsRequestDetails::Surround { .. } => Some(AudioPortType::SURROUND),
+            #[cfg(feature = "ambisonic")]
+            AudioPortsRequestDetails::Ambisonic { .. } => Some(AudioPortType::AMBISONIC),
+        }
+    }
+
+    /// Get the requested channel count.
+    pub fn channel_count(&self) -> u32 {
+        match self {
+            AudioPortsRequestDetails::Mono => 1,
+            AudioPortsRequestDetails::Stereo => 2,
+            AudioPortsRequestDetails::Untyped { channels, .. } => *channels,
+            #[cfg(feature = "surround")]
+            AudioPortsRequestDetails::Surround { channels } => {
+                channels.len().try_into().unwrap_or(u32::MAX) // is there a better way?
+            }
+            #[cfg(feature = "ambisonic")]
+            AudioPortsRequestDetails::Ambisonic { channels, .. } => *channels,
+        }
+    }
+
+    /// # Safety
+    /// The user must ensure the provided pointers are valid for the lifetime of `'a`.
+    /// Additionally, the `port_details` pointer must point to a valid structure
+    /// corresponding to the given `port_type` and `channel_count`.
+    unsafe fn from_raw(
+        port_type: *const c_char,
+        port_details: *const c_void,
+        channels: u32,
+    ) -> Self {
+        // SAFETY: Pointer validity ensured by the caller.
+        let port_type = unsafe { AudioPortType::from_raw(port_type) };
+
+        if port_type == Some(AudioPortType::MONO) && channels == 1 {
+            return Self::Mono;
+        }
+
+        if port_type == Some(AudioPortType::STEREO) && channels == 2 {
+            return Self::Stereo;
+        }
+
+        #[cfg(feature = "surround")]
+        if port_type == Some(AudioPortType::SURROUND) && !port_details.is_null() {
+            // SAFETY: details pointer validity is ensured by the caller.
+            let channels = unsafe {
+                crate::surround::SurroundChannel::from_raw_slice(std::slice::from_raw_parts(
+                    port_details as *const u8,
+                    channels as usize,
+                ))
+            };
+
+            if let Some(channels) = channels {
+                return Self::Surround { channels };
+            }
+        }
+
+        #[cfg(feature = "ambisonic")]
+        if port_type == Some(AudioPortType::AMBISONIC) && !port_details.is_null() {
+            // SAFETY: Validity ensured by the caller.
+            let config =
+                unsafe { crate::ambisonic::AmbisonicConfig::from_raw(*(port_details as *const _)) };
+
+            if let Some(config) = config {
+                return Self::Ambisonic { config, channels };
+            }
+        }
+
+        // For any unsupported or unrecognized port type, we return an untyped request with the given channel count.
+        // TODO: Should we return an error instead in case of unknown port type/invalid port details?
+        Self::Untyped { channels }
+    }
+
+    fn raw_details(&self) -> *const c_void {
+        match self {
+            #[cfg(feature = "surround")]
+            AudioPortsRequestDetails::Surround { channels } => {
+                crate::surround::SurroundChannel::as_raw_slice(channels).as_ptr() as *const c_void
+            }
+            #[cfg(feature = "ambisonic")]
+            AudioPortsRequestDetails::Ambisonic { config, .. } => {
+                config.as_raw() as *const _ as *const c_void
+            }
+            _ => null(),
+        }
+    }
+}
+
+/// A request to configure a single audio port.
+#[derive(Copy, Clone, Debug)]
+#[repr(transparent)]
+pub struct AudioPortsRequest<'a>(clap_audio_port_configuration_request, PhantomData<&'a ()>);
+
+impl<'a> AudioPortsRequest<'a> {
+    /// Create a new audio port configuration request with the given details.
+    pub fn new(is_input: bool, port_index: u32, details: AudioPortsRequestDetails<'a>) -> Self {
+        Self(
+            clap_audio_port_configuration_request {
+                is_input,
+                port_index,
+                port_type: details
+                    .port_type()
+                    .map(AudioPortType::as_raw)
+                    .unwrap_or(std::ptr::null()),
+                channel_count: details.channel_count(),
+                port_details: details.raw_details(),
+            },
+            PhantomData,
+        )
+    }
+
+    /// Is this request for an input port?
+    pub fn is_input(&self) -> bool {
+        self.0.is_input
+    }
+
+    /// The port index to configure
+    pub fn port_index(&self) -> u32 {
+        self.0.port_index
+    }
+
+    /// Get the details of this request, including the requested port type and channel count.
+    pub fn details(&self) -> AudioPortsRequestDetails<'a> {
+        // SAFETY: The raw request is expected to be valid for the lifetime of self.
+        unsafe {
+            AudioPortsRequestDetails::from_raw(
+                self.0.port_type,
+                self.0.port_details,
+                self.0.channel_count,
+            )
+        }
+    }
+
+    /// Convert a slice of `AudioPortsRequest` to a slice of `clap_audio_port_configuration_request`.
+    #[inline]
+    pub fn as_raw_slice(
+        slice: &'a [AudioPortsRequest<'a>],
+    ) -> &'a [clap_audio_port_configuration_request] {
+        // SAFETY: Safe due to #[repr(transparent)]
+        unsafe { std::slice::from_raw_parts(slice.as_ptr() as *const _, slice.len()) }
+    }
+
+    /// Convert a slice of `clap_audio_port_configuration_request` to a slice of `AudioPortsRequest`.
+    #[inline]
+    pub fn from_raw_slice(
+        slice: &'a [clap_audio_port_configuration_request],
+    ) -> &'a [AudioPortsRequest<'a>] {
+        // SAFETY: Safe due to #[repr(transparent)]
+        unsafe { std::slice::from_raw_parts(slice.as_ptr() as *const _, slice.len()) }
+    }
+}
+
+#[cfg(feature = "clack-plugin")]
+mod plugin;
+#[cfg(feature = "clack-plugin")]
+pub use plugin::*;
+
+#[cfg(feature = "clack-host")]
+mod host;
